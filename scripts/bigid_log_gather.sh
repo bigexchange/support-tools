1986c80c44421568d6471c7fec0ca2bae60076aa
Avior Malkukian <aviorma@gmail.com>
2023-11-03 15:49:08 +0000

    Update bigid_log_gather.sh

--- scripts/bigid_log_gather.sh
+++ scripts/bigid_log_gather.sh
@@ -1,92 +1,83 @@
 #!/bin/bash
+# Script Name: podLogs.sh
 # Author: kmccready@bigid.com
+# Description: The following script streamlines gathering current or previous kubernetes pod logs.
 
-# Release Notes:
-# Version 1.0-1.3: Sept 1-5 2023
-# - Rough concept of the script.
-# - Basic commands laid out with no functions, to get an idea of how I wanted this script to run.
-# - Working script from top to bottom, but did not capture previous logs, or allow reiteration.
+# Previous Versions:
+    # Version 1.0-1.3 Sept 1-5 2023
+        # Rough concept of the script.
+        # Basic commands laid out with no functions, to get an idea of how I wanted this script to run.
+        # Working script from top to bottom, but did not capture previous logs, or allow reiteration. 
 
-# Version 2.0-2.6: Sept 5 - Oct 8 2023
-# - Re-wrote the script to use functions.
-# - Added the ability to grab previous logs from restarted pods.
-# - Added error detection for RBAC.
-# - Added error detection and fallbacks for most functions.
-# - Added the ability to select all pods.
-# - Added the ability to start the script back over from after namespace selection.
+    # Version 2.0-2.6 Sept 5 Oct 8 2023
+        # Re-wrote the script to use functions.
+        # Added the ability to grab previous logs from restarted pods.
+        # Added error detection for RBAC.
+        # Added error detection and fallbacks for most functions.
+        # Added the ability to select all pods.
+        # Added the ability to start the script back over from after namespace selection.
 
-# Version 3.0-3.7: Oct 9-20 2023
-# - Re-wrote the script from version two to be more linear.
-# - Functions now follow the general path and flow of the script to make it less messy.
-# - Added log storage directory location choice.
-# - Added a function to prevent running the same lines of code multiple times in different locations.
-# - Re-wrote the script to make it more organized and easier to follow.
-# - Added the prompt to automatically compress the logs that were just gathered.
-# - Fixed the Namespace issue with RBAC and how it would infinite loop no matter what was entered.
-# - Fixed the previous logs "Select_All" from gathering EVERY pod and not just the restarted ones.
-# - Fixed the getContainerLogs function to be formatted correctly with if-else statements.
+    # Version 3.0-3.7 Oct 9-20 2023
+        # Re-wrote the script from version two to be more linear.
+        # Functions now follow the general path and flow of the script to make it less messy.
+        # Added log storage directory location choice.
+        # Added a function to prevent running the same lines of code multiple times in different locations.
+        # Re-wrote the script to make it more organized and easier to follow.
+        # Added the prompt to automatically compress the logs that were just gathered.
+        # Fixed the Namespace issue with RBAC and how it would infinite loop no matter what was entered.
+        # Fixed the previous logs "Select_All" from gathering EVERY pod and not just the restarted ones.
+        # Fixed the getContainerLogs function to be formatted correctly with if else statement.
+    # Version 3.8 Oct 21 2023
+        # Added a feature to add the kubectl describe $pod to the end of the corresponding pods logs.
+            # To prevent further gating behind RBAC, this will only work if the user has describe privileges.
+                # kubectl describe $pod -n $NAMESPACE
+        # Had the logLocation display the exact path where it will be saved to instead of saying "TodaysDate"
 
-# Version 3.8: Oct 21 2023
-# - Added a feature to add the kubectl describe $pod to the end of the corresponding pod's logs.
-# - To prevent further gating behind RBAC, this will only work if the user has describe privileges.
-# - kubectl describe $pod -n $NAMESPACE
-# - Had the logLocation display the exact path where it will be saved to instead of saying "TodaysDate"
+# Current Version: 
+    # Version 3.9 Nov 2 2023
+        # Fixed the wording on all the select statements to be less confusing.
+        # Fixed the issue with the restarted pods not correctly looping through and a allowing the user to select the desired label.
 
-# Function to log messages with different levels
+#variables for colors
+reset="\e[0m"
+# red="\e[91m"
+green="\e[92m"
+cyan="\e[96m"
+magenta="\e[95m"
+
+# Bold High Intensity
+# BIBlack="\e[1;90m"      # Black
+BIRed="\e[1;91m"   # Red
+BIGreen="\e[1;92m" # Green
+BIYellow="\e[1;93m"     # Yellow
+# BIBlue="\e[1;94m"       # Blue
+BIPurple="\e[1;95m" # Purple
+BICyan="\e[1;96m"   # Cyan
+BIWhite="\e[1;97m"  # White
+
+#Blinking
+blink="\e[5m"
+
+# log is used to filter error statuses (essentially beefed up echo)
 log() {
-    local msg=$1
-    local level=$2
-    local reset="\e[0m"
-    local red="\e[91m"
-    local green="\e[92m"
-    local cyan="\e[96m"
-    local magenta="\e[95m"
-    local BIRed="\e[1;91m"
-    local BICyan="\e[1;96m"
-    local BIWhite="\e[1;97m"
-    local blink="\e[5m"
-
+    msg=$1
+    level=$2
     case $level in
-        "error")
-            msg="${BIRed}[ERROR] ${msg}${reset}"
-            ;;
+    "error")
+        msg="${BIRed}[ERROR] ${msg}${reset}"
+        ;;
     esac
-
     echo -e "${msg}"
 }
 
+# Global Variables
+NAMESPACE=""
+SELECTED_LABELS=""
+RESTARTEDPODSEXIST=false
+namespace_list=$(kubectl get namespaces -o custom-columns=:metadata.name --no-headers)
+namespace_list_exit_code=$?
 
-printHelp() {
-    echo "Usage: $0 [OPTIONS]"
-    echo "Options:"
-    echo "  --help     Display this help message"
-    echo "  -h, --h    Display this help message"
-    echo "  -v, --version  Display script version information"
-    echo "  -n, --namespace  Select a Kubernetes namespace"
-    echo "  -d, --directory  Choose a directory for log storage"
-    echo "  -l, --labels  Select labels for pod filtering"
-    echo "  -c, --containers  Select specific containers within pods"
-    echo "  -a, --all  Gather logs from all pods"
-}
-
-# Main script logic
-while [[ $# -gt 0 ]]; do
-    key="$1"
-    case $key in
-        -h|--help)
-            printHelp
-            exit 0
-            ;;
-        # Add more cases for other options if necessary
-        *)
-            # Unknown option
-            echo "Error: Unknown option: $1"
-            printHelp
-            exit 1
-            ;;
-    esac
-    shift
-done
+# We use the exit code to filter out when namespace permissions fail
 
 # Check if kubectl is available
 if ! command -v kubectl &>/dev/null; then
@@ -94,36 +85,19 @@ if ! command -v kubectl &>/dev/null; then
     exit 1
 fi
 
-# Global Variables
-NAMESPACE=""
-SELECTED_LABELS=""
-RESTARTED_PODS_EXIST=false
-NAMESPACE_LIST=$(kubectl get namespaces -o custom-columns=:metadata.name --no-headers)
-NAMESPACE_LIST_EXIT_CODE=$?
-
-# Check if namespace is valid
-isNamespaceValid() {
-    local getPod
-    getPod=$(kubectl get pods -n "$1" 2>&1)
-    if [[ "$getPod" == *"No resources found"* ]]; then
-        return 1
-    else
-        return 0
-    fi
-}
-
-# Function to select the namespace
+#1. Ask for the NameSpace
+# Namespace permission check
 selectNamespace() {
-    local manualNamespace
-    if [ "$NAMESPACE_LIST_EXIT_CODE" -gt 0 ]; then
+    if [ "$namespace_list_exit_code" -gt 0 ]; then
         log "Permission denied. You do not have the required permissions to list namespaces." "error"
         while true; do
-            read -rp "Enter a namespace manually: " manualNamespace
-            if kubectl get pods -n "$manualNamespace" &>/dev/null && isNamespaceValid "$manualNamespace"; then
-                NAMESPACE="$manualNamespace"
+            read -rp "Enter a namespace manually: " manual_namespace
+            if kubectl get pods -n "$manual_namespace" &>/dev/null; then
+                NAMESPACE="$manual_namespace"
                 break
             else
-                log "Namespace '$manualNamespace' does not exist or cannot be accessed. Please enter a valid namespace." "error"
+                log "Namespace '$manual_namespace' does not exist or cannot be accessed. Please enter a valid namespace. " "error"
+                continue
             fi
         done
     else
@@ -178,12 +152,15 @@ nameSpaceInput() {
     done
 }
 
-#2. Ask if you want to a currently existing directory. Else it will be saved to /tmp/<namespace>/<date>
+#2. Ask if you want to save to a currently existing directory. Else it will be saved to /tmp/<namespace>/<date>
 # Additional Log saving information and compression.
 
 logLocation() {
     logDir=""
-    log "${BICyan}Do you want to save logs to a specific directory? ${reset}(Default is ${BIYellow}/tmp/$NAMESPACE/bigidLogs_$(date +%d-%b-%Y))${reset} (y/N)"
+    log "\n${BIRed}====================================================================${reset}"
+    log "${BIWhite}Logs will be saved by default to ${BIGreen}/tmp/$NAMESPACE/bigidLogs_$(date +%d-%b-%Y))${reset}"
+    log "${BIRed}====================================================================${reset}"
+    log "${BICyan}Would you like to specify a different directory for saving logs? ${reset}(y/N)"
     read -r directoryChoice
     if [[ "$directoryChoice" = y ]]; then
         while true; do
@@ -213,10 +190,10 @@ logLocation() {
 
 echoRestartedPods() {
     local restartedPods
-    restartedPods=$(kubectl get pods --no-headers -n "$NAMESPACE" | awk '$4>0')
+    restartedPods=$(kubectl get pods -n "$NAMESPACE" | awk '$4>0')
     # this will only grab pods where restarts are greater than 0.
     if [[ -n "$restartedPods" ]]; then
-        log "\n\t\t${BIGreen}${blink}The following pods have been restarted${reset}"
+        log "\t\t${BIGreen}${blink}The following pods have been restarted${reset}"
         log "${BIRed}========================================================================="
         log "${BIWhite}$restartedPods${reset}"
         log "${BIRed}=========================================================================\n"
@@ -233,11 +210,7 @@ echoRestartedPods() {
 selectLabels() {
     podLabels=""
     if [[ "$CHECKPREVIOUSLOGS" = true ]]; then
-        restartedPods=$(kubectl get pods --no-headers -n "$NAMESPACE" | awk '$4>0' | awk '{print $1}')
-        # for each restarted pod, get the SELECTED_LABELS
-        for pod in $restartedPods; do
-            podLabels="$(kubectl get pods "$pod" -n "$NAMESPACE" --no-headers -o custom-columns=":metadata.labels" | awk 'BEGIN {FS = "[[, ]"};{print$2}' | sed 's/:/=/g')"
-        done
+        podLabels=$(kubectl get pods -n $NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name,RESTARTS:.status.containerStatuses[0].restartCount,LABELS:metadata.labels' | awk '$2 != "<none>"' | awk '{print$3}' | awk 'BEGIN {FS = "[[, ]"};{print$2}' | sed 's/:/=/g' | sort -u)
     else
         # This gets the first unique label for each pod
         podLabels=$(kubectl get pods -n "$NAMESPACE" --no-headers -o custom-columns=":metadata.labels" | awk 'BEGIN {FS = "[[, ]"};{print$2}' | sed 's/:/=/g' | sort -u)
@@ -259,6 +232,7 @@ selectLabels() {
         fi
     done
 }
+
 
 #5. Ask for the Container if SelectAll is not chosen
 # function that drills down to the container level and grabs the logs
@@ -309,12 +283,12 @@ fetchLogs() {
         log "Grabbing logs from pod ${green}$pod${reset} and container ${magenta}${containerName}${reset} with the previous flag"
         filename="${pod}-${containerName}_Previous_$(date +%d-%b-%Y_%H_%M:%S).log"
         kubectl logs -n "$NAMESPACE" "$pod" -c "$containerName" --previous >>"${logDir}/${filename}" || log "Could not grab logs from pod $pod with the previous flag, container $containerName does not exist" "error"
-
+        log "Logs saved to ${green}${logDir}/${magenta}${filename}${reset}"
     else
         log "Grabbing logs from pod ${green}${pod}${reset} and container ${magenta}${containerName}${reset}"
         filename="${pod}-${containerName}-$(date +%d-%b-%Y_%H_%M:%S).log"
         kubectl logs -n "$NAMESPACE" "$pod" -c "$containerName" >>"${logDir}/${filename}" || log -e "Could not grab logs from pod $pod, container $containerName does not exist" "error"
-        log "Logs saved to ${cyan}${logDir}/${magenta}${filename}${reset}"
+        log "Logs saved to ${green}${logDir}/${magenta}${filename}${reset}"
     fi
     # Append kubectl describe to the end of the log file if they have permissions to do so.
     if [[ "$canDescribe" = true ]]; then
@@ -360,7 +334,8 @@ main() {
     # Main function where we will run through the script and allow for callbacks.
     CHECKPREVIOUSLOGS=false
     echoRestartedPods
-    log "${BICyan}Would you like to gather logs from all pods/pods of the same type ${BIWhite}(y),${BICyan} or just one pod? ${BIWhite}(default option)${reset} (y/N)"
+    log "${BIWhite}Logs will be gathered from ${BIRed}one${BIWhite} selected ${BIGreen}pod/container${BIWhite} by default.${reset}"
+    log "${BICyan}Do you want to grab logs from all pods of the same type instead?${BIGreen}(Example: all bigid-scanner pods)${reset} (y/N)"
     read -r labelAnswer
     if [[ "$labelAnswer" = "y" ]]; then
         selectLabels
@@ -442,7 +417,6 @@ main() {
 
     finishLine
 }
-
 selectNamespace
 logLocation
 main
